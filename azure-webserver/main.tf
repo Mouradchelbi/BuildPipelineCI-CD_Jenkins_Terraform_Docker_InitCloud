96b69b4135877a0f092c7cb2a6b5a7cc754e1cc5
Mouradchelbi <chelbimourad@gmail.com>
2023-06-15 21:35:42 +0200

    1234 port update

--- azure-webserver/main.tf
+++ azure-webserver/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_network_security_group" "allowedports" {
        direction = "Inbound"
        access = "Allow"
        protocol = "Tcp"
-       source_port_range = "5000"
+       source_port_range = "*"
        destination_port_range = "1234"
        source_address_prefix = "*"
        destination_address_prefix = "*"
